31db7539237329996df39886299e0411d98a8dfe
PoojaNarawad <poojanarawad16@gmail.com>
2024-01-31 18:36:57 +0530

    updated node modules and package-lock.json file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  // env: {
-  //   NODE_ENV: process.env.NODE_ENV,
-  // },
+  
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
